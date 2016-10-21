4a4d909c2660b037b2182b8282a35b1afb1b4730
Sudarshan <sudar.theone@gmail.com>
2016-10-21 13:48:22 +0530

    added get for weights, biases, layers

--- network.go
+++ network.go
@@ -1,25 +1,42 @@
 package main
 
 import (
-	"fmt"
 	"math/rand"
 
 	"github.com/gonum/matrix/mat64"
 )
 
-type Network struct {
-	sizes  []int
-	layers int
-	biases *mat64.Dense
-	weights *mat64.Dense
-}
-
-func main() {
-	data := make([]float64, 36)
+func randMat(x, y int) *mat64.Dense {
+	data := make([]float64, x*y)
 	for i := range data {
 		data[i] = rand.NormFloat64()
 	}
+	return mat64.NewDense(x, y, data)
+}
 
-	a := mat64.NewDense(6, 6, data)
-	fmt.Println(a)
+type Network struct {
+	Sizes   []int
+	Layers  int
+	Biases  []*mat64.Dense
+	Weights []*mat64.Dense
+}
+
+func(n *Network)getLayers(){
+	n.Layers = len(n.Sizes)
+}
+
+func(n *Network) getBiases(){
+	var ret []*mat64.Dense
+	for _ , vals := range n.Sizes[1:]{
+		ret = append(ret, randMat(vals, 1))
+	}
+	n.Biases = ret
+}
+
+func(n *Network)getWeights(){
+	var ret []*mat64.Dense
+	for i := range n.Sizes[: n.Layers - 1]{
+		ret = append(ret, randMat(n.Sizes[i], n.Sizes[i+1]))
+	}
+	n.Weights = ret
 }
